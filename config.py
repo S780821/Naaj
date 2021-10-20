329561022cc2ad67e801e3feabc6c9eed820c276
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-10-20 18:33:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "AQABxv9zwK3T0XoMfegbHMfVQxJOM0L7H-iKTjSF6Dyyu3OVbVsFHh6q70o-Jkdo1im9cImpScHz9vbCcyUab0SRow7mPds8415eaOhmOQnonljgeFpgGFP4Kh7NUPT9w12zzblocsuDEINySQhfzH6TfXU6qM-9BHWHPjj64yeY20rU7e8QVe61ShJZvb2Yu4zaDG-l9vCclbkBaJvseuTusRRlbGtO5Hp3GDrDvG0zypYVlg09ZlVWs_N-N3ivY0iR_YlGhQh2M8ZE3DIfyz8F4sd0M79bIJCfxzEFxTOH78IO3g5ZmfDNj-fqfhge-N1lYMxm5NuLJdamxYr19ljWeG5_-gA")
-BOT_TOKEN = getenv("BOT_TOKEN", "2082128018:AAGn-N-B4_wZWSwUmYiIIcsCsLq3zCWM-i0")
-BOT_NAME = getenv("BOT_NAME", "ᏔᎪᎡΝᎬᎡ ᎷႮՏᏆᏟ ")
-BOT_USERNAME = getenv("BOT_USERNAME", "WarnerMusic_bot")
+SESSION_NAME = getenv("SESSION_NAME", "AQBNiSPW4KrGqhBREG0mfR9gkwyiAUR98nuiZRsQPKTXpp3BU8DATptzUdiTFihq2RBodyQ0OQz_LDLNvNTYdRhMCxpTJMVR512JY5jflNATEOVTV-5Vv5eDURtzDn3Di8CBkewjIry6748bvU72RTx-rB5Zhc0lywPitJSUaRFw1aLVHT0h3t35P1Vsu0--bLCnipO0hm3u72UkBdC01OaEOh9e8JHu5wBSjPaNMZ3sOQ2_RQGf3UG4gF8tj9ICnrB4spNE7BdrPfraOcJpJlOpMhy3D4UpY488XZl8SV3UEJqux02ls1IAZ0Ssx-_fw5lBoIeqqPA0Xkpm5nMxhdv9dYEVUQA")
+BOT_TOKEN = getenv("BOT_TOKEN", "2057609602:AAFCFboy6H6-LjlXpMWCkU7Fuq-yms9hoJI")
+                  
+BOT_NAME = getenv("BOT_NAME", "ᏔᎪᎡΝᎬᎡ ᎷႮՏᏆᏟ V1 ")
+BOT_USERNAME = getenv("BOT_USERNAME", "WarnerMusic2_bot")
 API_ID = int(getenv("API_ID", "4856470"))
 API_HASH = getenv("API_HASH", "6dfa58833706557fb52017882bdb1abb")
 
